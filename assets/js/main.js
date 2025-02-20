fc922eacc3145eac738470e596ced6d9722daba7
AlexAlarcon <125610346+HALEXALARCON@users.noreply.github.com.>
2025-02-20 15:49:27 -0500

    portfolio-gen42-finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ import loader from './helpers/loader.js';
 import resetToHome from './helpers/reload_page.js';
 import activeMenu from './helpers/selected_menu.js';
 import sendEmail from './helpers/send_form.js';
+import changeLanguage from './helpers/Language.js';
 
 loader();
 resetToHome();
@@ -11,3 +12,4 @@ updateCurrentYear();
 parallax();
 activeMenu();
 sendEmail();
+changeLanguage();
